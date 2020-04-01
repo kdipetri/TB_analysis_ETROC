bbe88885d054449ea2cf585813b6b90b82b869c4
Karri DiPetrillo <kdipetri@github.com>
2020-04-01 17:21:52 -0500

    adding toa v amplitude cross check to etroc

--- scripts/landau.py
+++ scripts/landau.py
@@ -33,13 +33,15 @@ def get_amp_str(cfg,ch):
     return amp_str
 
 def get_slice( split ) :
-    if split == 0: return 15,17
-    if split == 1: return 17,18
-    if split == 2: return 18,19
-    if split == 3: return 19,20
-    if split == 4: return 20,21
-    if split == 5: return 22,23
-    if split == 6: return 23,25 
+    if split == 0: return 10,15
+    if split == 1: return 15,17
+    if split == 2: return 17,19
+    if split == 3: return 19,21
+    if split == 4: return 21,23
+    if split == 5: return 23,25
+    if split == 6: return 25,30
+    #if split == 7: return 22,23
+    #if split == 8: return 23,25 
     return -1,-1
 
 def get_slice_string(split,ch): 
@@ -73,7 +75,7 @@ def get_charge_channel(tree,cfg,ch,outfile):
     minAmp = get_min_amp(ch,cfg)
     transimpedance = 33000 # SF to make charge match UCSC 
 
-    hist = ROOT.TH1D("h_charge","",50,0,100)
+    hist = ROOT.TH1D("h_charge","",100,0,100)
 
     tree.Project("h_charge","-1000*integral[%i]*1e9*50/%f"%(ch,transimpedance),"amp[%i]>%f&&amp[3]>%i"%(ch,minAmp,minPh))
     
@@ -117,6 +119,78 @@ def get_amplitudes(tree,cfg,ch,outfile):
 
     return hists,means,errs
 
+def get_risetimes(tree,cfg,ch,outfile):
+
+    hists = []
+    means = []
+    errs = []
+    for i in range(0,7):
+        slice_str = get_slice_string(i,ch)
+        amp_str = get_amp_str(cfg,ch)
+
+        hist = ROOT.TH1D("h_risetime_{}".format(i),"",100,0,2)
+        tree.Project("h_risetime_{}".format(i),"1e9*abs(amp[%i]/risetime[%i])"%(ch,ch),"{}&&{}".format(slice_str,amp_str))
+
+        hist.SetTitle(";Risetime [ns];Events")
+        hist.SetLineColor(colors[i])
+    
+        outfile.cd()
+        hist.Write()
+
+        means.append(hist.GetMean())
+        errs.append(hist.GetMeanError())
+        hists.append(hist)
+
+    return hists,means,errs
+
+def get_slewrates(tree,cfg,ch,outfile):
+
+    hists = []
+    means = []
+    errs = []
+    for i in range(0,7):
+        slice_str = get_slice_string(i,ch)
+        amp_str = get_amp_str(cfg,ch)
+
+        hist = ROOT.TH1D("h_slewrate_{}".format(i),"",100,0,1000)
+        tree.Project("h_slewrate_{}".format(i),"1e-9*abs(risetime[%i])"%(ch),"{}&&{}".format(slice_str,amp_str))
+
+        hist.SetTitle(";Slewrate [mV/ns];Events")
+        hist.SetLineColor(colors[i])
+    
+        outfile.cd()
+        hist.Write()
+
+        means.append(hist.GetMean())
+        errs.append(hist.GetMeanError())
+        hists.append(hist)
+
+    return hists,means,errs
+
+def get_tots(tree,cfg,ch,outfile):
+
+    hists = []
+    means = []
+    errs = []
+    for i in range(0,7):
+        slice_str = get_slice_string(i,ch)
+        amp_str = get_amp_str(cfg,ch)
+
+        hist = ROOT.TH1D("h_tot_{}".format(i),"",100,0,20)
+        tree.Project("h_tot_{}".format(i),"1e9*tot_30[%i]"%(ch),"{}&&{}".format(slice_str,amp_str))
+
+        hist.SetTitle(";TOT [ns];Events")
+        hist.SetLineColor(colors[i])
+    
+        outfile.cd()
+        hist.Write()
+
+        means.append(hist.GetMean())
+        errs.append(hist.GetMeanError())
+        hists.append(hist)
+
+    return hists,means,errs
+
 def get_times(tree,cfg,ch,outfile):
 
     minT = get_min_time(ch,cfg)
@@ -135,12 +209,13 @@ def get_times(tree,cfg,ch,outfile):
         tree.Project("h_time_{}".format(i),"LP2_15[%i]-LP2_20[%i]"%(ch,ch_photek),"{}&&{}&&{}".format(sel_str,slice_str,amp_str))
 
         hist.SetTitle(";t_{0}-t_{ref} [s];Events")
+        outfile.cd()
+        hist.Write()
         
         f1 = ROOT.TF1("f_time_{}".format(i),"gaus",minT,maxT)
         hist.Fit(f1,"Q")
 
         outfile.cd()
-        hist.Write()
         f1.Write()
         
         hists.append(hist)
@@ -151,6 +226,36 @@ def get_times(tree,cfg,ch,outfile):
     
     return hists,means,errs,tres,tres_err
 
+def check_amp_TOA(tree,cfg,ch,outfile):
+    # no sel string
+    amp_str = get_amp_str(cfg,ch)
+    minT = get_min_time(ch,cfg)
+    maxT = get_max_time(ch,cfg)
+    hist = ROOT.TH2D("h_TOA_v_amp","",100,0,750,100,minT,maxT)
+    
+    sel_str = "amp[%i]>%f && amp[%i]<%f && LP2_15[%i]!=0 && LP2_20[%i]!=0"%(ch_photek,minPh,ch_photek,maxPh,ch,ch_photek)
+    #tree.Project("h_TOA_v_amp","LP2_30[%i]-LP2_20[%i]:amp[%i]"%(ch,ch_photek,ch),"{}&&{}".format(sel_str,amp_str))
+    tree.Project("h_TOA_v_amp","LP2_15[%i]-LP2_20[%i]:amp[%i]"%(ch,ch_photek,ch),"{}&&{}".format(sel_str,amp_str))
+
+    hist.SetTitle(";Amplitude [mV];t_{0}-t_{ref} [s]")
+
+    c = ROOT.TCanvas()
+    hist.Draw("COLZ")
+    profile = hist.ProfileX("h_timewalkprof")
+    profile.SetMarkerStyle(20);
+    profile.SetMarkerSize(0.8);
+    profile.SetMarkerColor(ROOT.kBlack);
+    profile.SetLineColor(ROOT.kBlack);
+    profile.SetLineWidth(1);
+    profile.Draw("same")
+    c.Print("plots/landau/{}_{}_tdiff_v_amp.pdf".format(cfg,ch))
+    
+    outfile.cd()
+    hist.Write()
+
+    return hist
+
+    
 def draw_hists(cfg,ch,hists,name):
     c = ROOT.TCanvas()
     ymax = 0
@@ -198,7 +303,7 @@ def draw_graph(cfg,ch,y,yerr,outfile,name):
     outfile.cd()
     g.Write()
 
-    return
+    return g
 
 def plot_overlay(outfile, grs, biases, boards, temps, configs, name): 
     c = ROOT.TCanvas()
@@ -206,24 +311,31 @@ def plot_overlay(outfile, grs, biases, boards, temps, configs, name):
     c.SetGridx()
 
     mgraph = ROOT.TMultiGraph()
-    leg = ROOT.TLegend(0.17,0.71,0.56,0.88)
+    leg = ROOT.TLegend(0.17,0.71,0.63,0.88)
     leg.SetMargin(0.15)
 
     for i,graph in enumerate(grs):
-        cosmetics(graph,i)
+        cosmetics(graph,i+1)
         mgraph.Add(graph)
-        label = "ETROC#{}, {}V, {}C".format(boards[i],biases[i],temps[i])
-        if "0b000" in configs[i]: label = label + " High power"
-        if "0b111" in configs[i]: label = label + " Low power"
+        cfg = ""
+        if "0b000" in configs[i]: cfg = " high power"
+        if "0b111" in configs[i]: cfg = " low power"
+        label = "ETROC #{}, {}V, {}, {}C".format(boards[i],biases[i],cfg,temps[i])
         leg.AddEntry(graph, label ,"EP")
     
     yaxis=""
-    if "amp" in name: yaxis = "MPV amplitude [mV]"
+    if "amp" in name: yaxis = "mean amplitude [mV]"
     if "tdiff" in name: yaxis = "mean t_{0} - t_{ref} (CFD) [ns]"
     if "tres" in name: yaxis = "amplifier time res. (CFD) [ns]"
+    if "rise" in name: yaxis = "mean risetime [ns]"
+    if "slew" in name: yaxis = "mean slewrate [mV/ns]"
+    if "tot" in name: yaxis = "mean TOT [ns]"
     mgraph.SetTitle("; collected charge [fC]; %s"%(yaxis) )
     mgraph.Draw("ALEP")
-    adjust(mgraph,gr_name)
+    #adjust(mgraph,gr_name)
+    ymax = -1
+    if "tdiff" in name : mgraph.GetHistogram().GetYaxis().SetRangeUser(3.8,4.5) 
+    elif "rise" in name: mgraph.GetHistogram().GetYaxis().SetRangeUser(0.8,1.4) 
     leg.Draw() 
     c.Print("plots/landau/series_{}.pdf".format(name))
     
@@ -239,8 +351,11 @@ def get_configs():
     
 
     gr_amps = []
+    gr_rises = []
+    gr_slews = []
     gr_tdiffs = []
     gr_tress = []
+    gr_tots = []
     cfg_list = open("configs/configurations_landau.txt","r")
     for line in cfg_list: 
         if "#" in line: continue
@@ -260,25 +375,43 @@ def get_configs():
 
         get_charge_channel(tree,cfg,ch,outfile) 
         hists_amp,mean_amps,err_amps = get_amplitudes(tree,cfg,ch,outfile)
+        hists_rise,mean_rises,err_rises = get_risetimes(tree,cfg,ch,outfile)
+        hists_slew,mean_slews,err_slews = get_slewrates(tree,cfg,ch,outfile)
+        hists_tot ,mean_tots ,err_tots  = get_tots(tree,cfg,ch,outfile)
         hists_time,mean_times,err_times,tres,err_tres = get_times(tree,cfg,ch,outfile)
+    
+        # checks
+        check_amp_TOA(tree,cfg,ch,outfile)
         
         # draw
         draw_hists(cfg,ch,hists_amp,"amplitude")
+        draw_hists(cfg,ch,hists_rise,"risetime")
+        draw_hists(cfg,ch,hists_slew,"slewrate")
         draw_hists(cfg,ch,hists_time,"time")
+        draw_hists(cfg,ch,hists_tot,"tot")
 
         # graph
         gr_amp   = draw_graph(cfg,ch,mean_amps ,err_amps ,outfile,"amp")
+        gr_rise  = draw_graph(cfg,ch,mean_rises,err_rises,outfile,"risetime")
+        gr_slew  = draw_graph(cfg,ch,mean_slews,err_slews,outfile,"slewrate")
         gr_tdiff = draw_graph(cfg,ch,mean_times,err_times,outfile,"tdiff")
         gr_tres  = draw_graph(cfg,ch,tres      ,err_tres ,outfile,"tres")
+        gr_tot   = draw_graph(cfg,ch,mean_tots ,err_tots ,outfile,"tot")
     
         gr_amps.append(gr_amp)
+        gr_rises.append(gr_rise)
+        gr_slews.append(gr_slew)
         gr_tdiffs.append(gr_tdiff)
         gr_tress.append(gr_tres)
+        gr_tots.append(gr_tot)
 
     # done with cfgs
-    plot_overlay(outfile, gr_amps  , biases, boards, temps, cfg, "amp") 
-    plot_overlay(outfile, gr_tdiffs, biases, boards, temps, cfg, "tdiff") 
-    plot_overlay(outfile, gr_tress , biases, boards, temps, cfg, "tres") 
+    plot_overlay(outfile, gr_amps  , biases, boards, temps, configs, "amp") 
+    plot_overlay(outfile, gr_rises , biases, boards, temps, configs, "risetime") 
+    plot_overlay(outfile, gr_slews , biases, boards, temps, configs, "slewrate") 
+    plot_overlay(outfile, gr_tdiffs, biases, boards, temps, configs, "tdiff") 
+    plot_overlay(outfile, gr_tress , biases, boards, temps, configs, "tres") 
+    plot_overlay(outfile, gr_tots  , biases, boards, temps, configs, "tot") 
     return 
 
 get_configs()
